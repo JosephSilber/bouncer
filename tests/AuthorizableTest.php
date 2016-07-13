ab5e304d1855d206b975dd9b835d9a6e2ca292ee
Joseph Silber <contact@josephsilber.com>
2016-07-12 22:23:20 -0400

    Also test model abilities

--- tests/AuthorizableTest.php
+++ tests/AuthorizableTest.php
@@ -4,7 +4,7 @@ use Silber\Bouncer\Database\Role;
 
 class AuthorizableTest extends BaseTestCase
 {
-    public function testCheckingAbilitiesOnRole()
+    public function testCheckingSimpleAbilitiesOnRoles()
     {
         $role = Role::create(['name' => 'admin']);
 
@@ -13,5 +13,17 @@ class AuthorizableTest extends BaseTestCase
         $this->assertTrue($role->can('scream'));
         $this->assertTrue($role->cant('shout'));
         $this->assertTrue($role->cannot('cry'));
+    }
+
+    public function testCheckingModelAbilitiesOnRoles()
+    {
+        $role = Role::create(['name' => 'admin']);
+
+        $role->allow('create', User::class);
+
+        $this->assertTrue($role->can('create', User::class));
+        $this->assertTrue($role->cannot('create', Account::class));
+        $this->assertTrue($role->cannot('create'));
+        $this->assertTrue($role->cannot('update', User::class));
     }
 }
