151094d8be4e7950b427efb6b78b2113a4712064
Joseph Silber <contact@josephsilber.com>
2016-07-13 21:16:03 -0400

    Create IsRole and IsAbility traits

---
+++ src/Database/IsAbility.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Silber\Bouncer\Database;
+
+use App\User;
+use Silber\Bouncer\Database\Constraints\AbilitiesForModel;
+
+trait IsAbility
+{
+    /**
+     * Constructor.
+     *
+     * @param array  $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->table = Models::table('abilities');
+
+        parent::__construct($attributes);
+    }
+
+    /**
+     * Create a new ability for a specific model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model|string  $model
+     * @param  string  $name
+     * @return static
+     */
+    public static function createForModel($model, $name)
+    {
+        if ($model == '*') {
+            return static::createForModelWildcard($name);
+        }
+
+        return static::forceCreate([
+            'name'        => $name,
+            'entity_type' => $model->getMorphClass(),
+            'entity_id'   => $model->exists ? $model->getKey() : null,
+        ]);
+    }
+
+    /**
+     * Create a new ability for all models.
+     *
+     * @param  string  $name
+     * @return static
+     */
+    public static function createForModelWildcard($name)
+    {
+        return static::forceCreate([
+            'name'        => $name,
+            'entity_type' => '*',
+        ]);
+    }
+
+    /**
+     * The roles relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+     */
+    public function roles()
+    {
+        return $this->morphedByMany(
+            Models::classname(Role::class),
+            'entity',
+            Models::table('permissions')
+        );
+    }
+
+    /**
+     * The users relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+     */
+    public function users()
+    {
+        return $this->morphedByMany(
+            Models::classname(User::class),
+            'entity',
+            Models::table('permissions')
+        );
+    }
+
+    /**
+     * Get the identifier for this ability.
+     *
+     * @return string
+     */
+    final public function getIdentifierAttribute()
+    {
+        $slug = $this->attributes['name'];
+
+        if ($this->attributes['entity_type']) {
+            $slug .= '-'.$this->attributes['entity_type'];
+        }
+
+        if ($this->attributes['entity_id']) {
+            $slug .= '-'.$this->attributes['entity_id'];
+        }
+
+        return strtolower($slug);
+    }
+
+    /**
+     * Get the ability's "slug" attribute.
+     *
+     * @return string
+     */
+    public function getSlugAttribute()
+    {
+        return $this->getIdentifierAttribute();
+    }
+
+    /**
+     * Constrain a query to having the given name.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
+     * @return string|array  $name
+     * @return bool  $strict
+     * @return void
+     */
+    public function scopeByName($query, $name, $strict = false)
+    {
+        $names = (array) $name;
+
+        if ( ! $strict) {
+            $names[] = '*';
+        }
+
+        $query->whereIn("{$this->table}.name", $names);
+    }
+
+    /**
+     * Constrain a query to simple abilities.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
+     * @return void
+     */
+    public function scopeSimpleAbility($query)
+    {
+        $query->whereNull("{$this->table}.entity_type");
+    }
+
+    /**
+     * Constrain a query to an ability for a specific model.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder  $query
+     * @param  \Illuminate\Database\Eloquent\Model|string  $model
+     * @param  bool  $strict
+     * @return void
+     */
+    public function scopeForModel($query, $model, $strict = false)
+    {
+        (new AbilitiesForModel)->constrain($query, $model, $strict);
+    }
+}
---
+++ src/Database/IsRole.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Silber\Bouncer\Database;
+
+use Silber\Bouncer\Helper;
+use Silber\Bouncer\Database\Constraints\Roles as RolesConstraint;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+trait IsRole
+{
+    use HasAbilities, Authorizable {
+        HasAbilities::getClipboardInstance insteadof Authorizable;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param array  $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->table = Models::table('roles');
+
+        parent::__construct($attributes);
+    }
+
+    /**
+     * The users relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphedByMany
+     */
+    public function users()
+    {
+        return $this->morphedByMany(
+            Models::classname(User::class),
+            'entity',
+            Models::table('assigned_roles')
+        );
+    }
+
+    /**
+     * Assign the role to the given model(s).
+     *
+     * @param  string|\Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection  $model
+     * @param  array|null  $keys
+     * @return $this
+     */
+    public function assignTo($model, array $keys = null)
+    {
+        list($model, $keys) = Helper::extractModelAndKeys($model, $keys);
+
+        $query = $this->newBaseQueryBuilder()->from(Models::table('assigned_roles'));
+
+        $query->insert($this->createAssignRecords($model, $keys));
+
+        return $this;
+    }
+
+    /**
+     * Retract the role from the given model(s).
+     *
+     * @param  string|\Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection  $model
+     * @param  array|null  $keys
+     * @return $this
+     */
+    public function retractFrom($model, array $keys = null)
+    {
+        list($model, $keys) = Helper::extractModelAndKeys($model, $keys);
+
+        $this->newBaseQueryBuilder()
+             ->from(Models::table('assigned_roles'))
+             ->where('role_id', $this->getKey())
+             ->where('entity_type', $model->getMorphClass())
+             ->whereIn('entity_id', $keys)
+             ->delete();
+
+        return $this;
+    }
+
+    /**
+     * Create the pivot table records for assigning the role to given models.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  array  $keys
+     * @return array
+     */
+    protected function createAssignRecords(Model $model, array $keys)
+    {
+        $type = $model->getMorphClass();
+
+        return array_map(function ($key) use ($type) {
+            return [
+                'role_id'     => $this->getKey(),
+                'entity_type' => $type,
+                'entity_id'   => $key,
+            ];
+        }, $keys);
+    }
+
+    /**
+     * Constrain the given query to roles that were assigned to the given authorities.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string|\Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection  $model
+     * @param  array  $keys
+     * @return void
+     */
+    public function scopeWhereAssignedTo($query, $model, array $keys = null)
+    {
+        (new RolesConstraint)->constrainWhereAssignedTo($query, $model, $keys);
+    }
+}
