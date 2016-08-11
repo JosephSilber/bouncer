7aa22c5b2f3d29d31362563aa29236704c0d44b2
Joseph Silber <contact@josephsilber.com>
2016-08-10 21:20:13 -0400

    Add level to fillable

--- src/Database/Role.php
+++ src/Database/Role.php
@@ -13,7 +13,7 @@ class Role extends Model
      *
      * @var array
      */
-    protected $fillable = ['name', 'title'];
+    protected $fillable = ['name', 'title', 'level'];
 
     /**
      * Constructor.
