4afbb129069b84bba40a966bc953593c6732d1aa
JeroenG <jeroengjeroeng@gmail.com>
2017-03-28 21:03:41 +0200

    Refactor tests and add (un)forbid multiple abilities test

--- tests/MultipleAbilitiesTest.php
+++ tests/MultipleAbilitiesTest.php
@@ -3,6 +3,10 @@
 class MultipleAbilitiesTest extends BaseTestCase
 {
 
+    /**
+     * When set to true, queries are counted and reported for tests in this file.
+     * @var boolean
+     */
     public $report = false;
 
     public function setUp()
@@ -23,47 +27,9 @@ class MultipleAbilitiesTest extends BaseTestCase
         parent::tearDown();
     }
 
-    public function test_control_behaviour()
+    public function test_multiple_abilities()
     {
-        $user = User::create();
-
-        $bouncer = $this->bouncer($user)->dontCache();
-
-        $bouncer->allow($user)->to('edit');
-        $bouncer->allow($user)->to('delete');
-
-        $this->assertTrue($bouncer->allows('edit'));
-        $this->assertTrue($bouncer->allows('delete'));
-
-        $bouncer->disallow($user)->to('edit');
-
-        $this->assertTrue($bouncer->denies('edit'));
-        $this->assertTrue($bouncer->allows('delete'));
-    }
-
-    public function test_control_behaviour_with_model_reference()
-    {
-        $user = User::create();
-
-        $bouncer = $this->bouncer($user)->dontCache();
-
-        $bouncer->allow($user)->to('edit', User::class);
-        $bouncer->allow($user)->to('delete', User::class);
-
-        $this->assertTrue($bouncer->allows('edit', User::class));
-        $this->assertTrue($bouncer->allows('delete', User::class));
-
-        $bouncer->disallow($user)->to('edit', User::class);
-
-        $this->assertTrue($bouncer->denies('edit', User::class));
-        $this->assertTrue($bouncer->allows('delete', User::class));
-    }
-
-    public function test_multiple_abilties()
-    {
-        $user = User::create();
-
-        $bouncer = $this->bouncer($user)->dontCache();
+        $bouncer = $this->bouncer($user = User::create())->dontCache();
 
         $bouncer->allow($user)->to(['edit', 'delete']);
 
@@ -78,9 +44,7 @@ class MultipleAbilitiesTest extends BaseTestCase
 
     public function test_multiple_abilties_with_model_reference()
     {
-        $user = User::create();
-
-        $bouncer = $this->bouncer($user)->dontCache();
+        $bouncer = $this->bouncer($user = User::create())->dontCache();
 
         $bouncer->allow($user)->to(['edit', 'delete'], User::class);
 
@@ -91,5 +55,66 @@ class MultipleAbilitiesTest extends BaseTestCase
 
         $this->assertTrue($bouncer->denies('edit', User::class));
         $this->assertTrue($bouncer->allows('delete', User::class));
+    }
+
+    public function test_allow_multiple_abilties()
+    {
+        $bouncer = $this->bouncer($user = User::create())->dontCache();
+
+        $bouncer->allow($user)->to(['edit', 'delete']);
+
+        $this->assertTrue($bouncer->allows('edit'));
+        $this->assertTrue($bouncer->allows('delete'));
+    }
+
+    public function test_disallow_multiple_abilties()
+    {
+        $bouncer = $this->bouncer($user = User::create())->dontCache();
+
+        $bouncer->disallow($user)->to(['edit', 'delete']);
+
+        $this->assertTrue($bouncer->denies('edit'));
+        $this->assertTrue($bouncer->denies('delete'));
+    }
+
+    public function test_forbid_multiple_abilities()
+    {
+        $bouncer = $this->bouncer($user = User::create())->dontCache();
+
+        $bouncer->allow($user)->to(['edit', 'delete']);
+        $bouncer->forbid($user)->to(['edit', 'delete']);
+
+        $this->assertTrue($bouncer->denies('edit'));
+        $this->assertTrue($bouncer->denies('delete'));
+    }
+
+    public function test_unforbid_multiple_abilities()
+    {
+        $bouncer = $this->bouncer($user = User::create())->dontCache();
+
+        $bouncer->allow($user)->to(['edit', 'delete']);
+        $bouncer->forbid($user)->to(['edit', 'delete']);
+        
+        $this->assertTrue($bouncer->denies('edit'));
+
+        $bouncer->unforbid($user)->to(['edit', 'delete']);
+
+        $this->assertTrue($bouncer->allows('edit'));
+        $this->assertTrue($bouncer->allows('delete'));
+    }
+
+    public function test_forbid_multiple_abilities_with_model_reference()
+    {
+        $bouncer = $this->bouncer($user = User::create())->dontCache();
+
+        $bouncer->allow($user)->to(['edit', 'delete'], User::class);
+        $bouncer->forbid($user)->to(['edit', 'delete'], User::class);
+
+        $this->assertTrue($bouncer->denies('delete', User::class));
+
+        $bouncer->unforbid($user)->to('delete', User::class);
+
+        $this->assertTrue($bouncer->allows('delete', User::class));
+        $this->assertFalse($bouncer->allows('edit', User::class));
     }
 }
