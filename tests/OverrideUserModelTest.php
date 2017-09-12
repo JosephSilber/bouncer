c45dc8dbf552dc2eb6c5923ed9423c6b1694354e
Patrick Stephan <pstephan1187@gmail.com>
2017-09-12 16:23:10 -0500

    Added tests for user override logic.

---
+++ tests/OverrideUserModelTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Silber\Bouncer\Database\Models;
+
+class OverrideUserModelTest extends BaseTestCase
+{
+    public function testModelsUsesAppUserByDefault()
+    {
+        $this->assertEquals('App\User', Models::getUserClass());
+    }
+
+    public function testCanOverrideModelsUserClassWithString()
+    {
+        Models::setUserClass(AnotherUserClass::class);
+
+        $this->assertEquals('AnotherUserClass', Models::getUserClass());
+    }
+
+    public function testCanOverrideModelsUserClassWithObject()
+    {
+        Models::setUserClass(new AnotherUserClass);
+
+        $this->assertEquals('AnotherUserClass', Models::getUserClass());
+    }
+}
+
+class AnotherUserClass
+{}
