34a5d3beb89de198fe1954e5eb66504776cbe2ce
Joseph Silber <contact@josephsilber.com>
2019-02-26 23:39:17 -0500

    Add support for Laravel 5.8

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -80,6 +80,10 @@ abstract class BaseTestCase extends TestCase
         $container->bind('db', function () {
             return $this->db();
         });
+
+        $container->bind('db.schema', function () {
+            return $this->db()->connection()->getSchemaBuilder();
+        });
     }
 
     protected function migrate()
