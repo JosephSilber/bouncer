ad75a150d6e78b66e7c7f8826a3300f9e65c0373
Joseph Silber <contact@josephsilber.com>
2020-01-14 20:16:43 -0500

    Remove scope removal
    In modern versions of Laravel, scopes are no longer removed.
    Instead, scopes are applied just in time right before any
    query is executed. Removing simply makes it never run.

--- src/Database/Scope/TenantScope.php
+++ src/Database/Scope/TenantScope.php
@@ -32,31 +32,4 @@ class TenantScope implements EloquentScope
     {
         Models::scope()->applyToModelQuery($query, $model->getTable());
     }
-
-    /**
-     * Remove the scope from the given Eloquent query builder.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @return void
-     */
-    public function remove(Builder $query, Model $model)
-    {
-        $query = $query->getQuery();
-
-        $query->wheres = (new Collection($query->wheres))->reject(function ($where) {
-            return $this->isScopeConstraint($where);
-        })->values()->all();
-    }
-
-    /**
-     * Determine whether the given "where" is a scope constraint.
-     *
-     * @param  array  $where
-     * @return bool
-     */
-    protected function isScopeConstraint($where)
-    {
-        return $where['type'] == 'Null' && $where['column'] == 'scope';
-    }
 }
